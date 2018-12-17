2f4db8fd119069641739c96887c931e77bcf3b2f
Matt Magoffin <git+matt@msqr.us>
2018-12-18 10:21:15 +1300

    Refine error handling. Make topic configurable.

--- src/solarflux-demo.js
+++ src/solarflux-demo.js
@@ -9,14 +9,14 @@ import CBOR from "cbor-sync";
 import connectionOptions from "./conn-options";
 
 const fluxEnv = new Environment({
+  /*
   protocol: "ws",
   host: "flux.solarnetworkdev.net",
   port: 9001
-  /*
+  */
   protocol: "wss",
   host: "flux.solarnetwork.net",
   port: 443
-  */
 });
 
 const snEnv = new Environment({
@@ -28,6 +28,19 @@ const snEnv = new Environment({
 });
 
 var app;
+
+function bytesToHex(bytes) {
+  var s = "",
+    b;
+  for (var i = 0; i < bytes.length; i += 1) {
+    b = bytes[i];
+    if (b < 0x10) {
+      s += "0";
+    }
+    s += b.toString(16);
+  }
+  return s;
+}
 
 /**
  * SolarFlux demo app.
@@ -48,23 +61,10 @@ var fluxApp = function(fluxEnvironment, snEnvironment, options) {
   var client;
 
   function onConnectionLost(resp) {
-    select("#connect").attr("disabled", null);
+    uiStateConnected(false);
     if (resp.errorCode !== 0) {
       console.log("onConnectionLost:" + resp.errorMessage);
     }
-  }
-
-  function bytesToHex(bytes) {
-    var s = "",
-      b;
-    for (var i = 0; i < bytes.length; i += 1) {
-      b = bytes[i];
-      if (b < 0x10) {
-        s += "0";
-      }
-      s += b.toString(16);
-    }
-    return s;
   }
 
   function onMessageArrived(message) {
@@ -83,7 +83,7 @@ var fluxApp = function(fluxEnvironment, snEnvironment, options) {
         console.log("Message does not appear to be CBOR: " + e);
       }
     }
-    console.log(message.destinationName + " message: " + body);
+    console.log(`${message.destinationName} message: %o`, body);
     select("#message-template").select(function() {
       var msgEl = this.cloneNode(true);
       select(msgEl)
@@ -168,9 +168,18 @@ var fluxApp = function(fluxEnvironment, snEnvironment, options) {
     }
   }
 
+  function uiStateConnected(connected, fully) {
+    select("#connect").attr("disabled", connected ? "disabled" : null);
+    select("#end").attr("disabled", !connected ? "disabled" : null);
+    select("#topics").attr("disabled", connected ? "disabled" : null);
+
+    selectAll(".output").classed("disabled", !(connected && fully));
+
+    selectAll(".hide-after-auth").classed("hidden", connected && fully);
+  }
+
   function connect() {
-    const connBtn = select("#connect");
-    connBtn.attr("disabled", "disabled");
+    uiStateConnected(true);
     const tokenId = select("input[name=token]").property("value");
 
     var options = connectionOptions(tokenId, select("input[name=secret]").property("value"));
@@ -189,23 +198,58 @@ var fluxApp = function(fluxEnvironment, snEnvironment, options) {
     client.onConnectionLost = onConnectionLost;
     client.onMessageArrived = onMessageArrived;
 
+    let topics = document.getElementById("topics").value;
+    if (!topics) {
+      topics = "node/+/datum/0/#";
+    }
+    let subOptions = {
+      onSuccess: subscribeSuccess,
+      onFailure: subscribeError
+    };
+
     client.connect(options);
 
     function connectSuccess(json) {
       console.log(`Connected to MQTT on ${client.host}:${client.port}${client.path}`);
-      client.subscribe("node/+/datum/0/#");
-      selectAll(".hide-after-auth").classed("hidden", true);
-      select("section.output").classed("disabled", false);
+      console.log(`Subscribing to topics: ${topics}`);
+      client.subscribe(topics, subOptions);
     }
 
     function connectError(error) {
-      connBtn.attr("disabled", null);
-      console.log("conn error " + error.code + ": " + error.message);
+      uiStateConnected(false);
+      let msg = `Error connecting to ${fluxEnvironment.host}:${fluxEnvironment.port} (${
+        error.errorCode
+      }): ${error.errorMessage}`;
+      console.error(msg);
+      alert(msg);
+    }
+
+    function subscribeSuccess() {
+      console.log("Subscribed to MQTT topics " + topics);
+      uiStateConnected(true, true);
+    }
+
+    function subscribeError(error) {
+      uiStateConnected(false);
+      let msg = `Subscribe error for topics [${topics}] (${error.errorCode}): ${
+        error.errorMessage
+      }`;
+      console.log(msg);
+      if (error.errorCode && error.errorCode.length > 0 && error.errorCode[0] === 128) {
+        // permission denied
+        msg = `Permission denied for topics [${topics}]. Check that the security policy for token [${tokenId}] allows access to the node and source IDs included in the topic pattern. Note that topic wildcards only work if the token's security policy does not restrict node or source IDs.`;
+      }
+      alert(msg);
     }
   }
 
   function init() {
-    select("#connect").on("click", connect);
+    document.getElementById("connect").addEventListener("click", connect);
+    document.getElementById("topic-form").addEventListener("submit", function(event) {
+      event.preventDefault();
+      return false;
+    });
+    select("#topic-form").on("submit");
     selectAll("input.auth").on("keyup", handleAuthorizationInputKeyup);
     return Object.defineProperties(self, {
       start: { value: start },
